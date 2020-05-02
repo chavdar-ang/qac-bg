1c3af7752122280e459edd8b31cd951867f6719f
Chavdar Angelov <chavdar84@gmail.com>
2020-05-02 12:34:00 +0300

    SQLSTATE[42000] fix.

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,3 +2,4 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('post') }}'><i class='nav-icon fa fa-question'></i> Posts</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('topic') }}'><i class='nav-icon fa fa-question'></i> Topics</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('inquiry') }}'><i class='nav-icon fa fa-question'></i> Inquiries</a></li>
--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -16,4 +16,5 @@ Route::group([
 ], function () { // custom admin routes
     Route::crud('post', 'PostCrudController');
     Route::crud('topic', 'TopicCrudController');
+    Route::crud('inquiry', 'InquiryCrudController');
 }); // this should be the absolute last line of this file
