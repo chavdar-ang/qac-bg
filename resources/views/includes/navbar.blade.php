b8f8398684b339066e96b8e9c3fc11ebefc2f59d
Viki <viki.radeva91@gmail.com>
2020-05-02 15:27:39 +0300

    Navbar merge fix.

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -6,15 +6,9 @@
         <div style="clear: both;"></div>
         <nav class="nav-mobile">
             <a href="/" class="fadein red">Начало</a>
-<<<<<<< HEAD
             <a href="{{ route('posts.index') }}" class="fadein red">За нас</a>
             <a href="#services" class="fadein red">Услуги</a>
             <!-- <a href="{{ route('posts.index') }}" class="fadein red">Блог</a> -->
-=======
-            <!-- <a href="{{ route('posts.index') }}" class="fadein red">За нас</a> -->
-            <a href="/#services" class="fadein red">Услуги</a>
-            <a href="{{ route('about.show') }}" class="fadein red">За нас</a>
->>>>>>> f970469065c07ead1c2469fcf2aab2f5d1d3f268
             <a href="{{ route('contacts.show') }}" class="fadein submit">Контакти</a>
             <div class="social">
                 <a class="fb" href="https://www.facebook.com/QACbg/" target="_blank"></a>
