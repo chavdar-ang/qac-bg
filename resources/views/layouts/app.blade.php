962279794f22b32c39875bc097fa3439b6331c74
Chavdar Angelov <chavdar84@gmail.com>
2020-05-06 17:51:17 +0300

    Meta description added.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Внедряване на международни стандарти, продуктова сертификация - маркировка СЕ и други маркировки.">
     <link rel="stylesheet" href="/css/app.css?v=2">
     <script src="/js/app.js?v=2"></script>
     <title>Кю Ей Си ООД - @yield('title')</title>
