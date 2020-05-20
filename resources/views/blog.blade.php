228a7d3adad004ab51e2e3551d5a7be526c596aa
Viki <viki.radeva91@gmail.com>
2020-05-20 16:45:45 +0300

    changes

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,41 +4,18 @@
 
 @section('content')
 
-<div class="container">
-    <div class="blog">
-        <div class="posts">
-            @forelse($posts as $post)
-            <li class="item">
-                <a href="{{ route('posts.show', $post->slug) }}" class="feature" style="background-image: url('https://picsum.photos/400/250?i={{ $post->id }}')" alt="{{ $post->title }}">
-                </a>
-                <div class="meta">Публикувана на {{ $post->date }}</div>
-                <h3 class="title">
-                    <a href="{{ route('posts.show', $post->slug) }}">{{ $post->title }}</a>
-                </h3>
-                <div class="excerpt">{{ $post->excerpt }}</div>
-                <div class="topics">
-                    @foreach($post->topics as $topic)
-                    <a href="{{ route('topics.show', $topic->slug) }}" class="topic">{{ $topic->name }}</a>
-                    @endforeach
-                </div>
-            </li>
-            @empty
-            <p>Няма налични публикации</p>
-            @endforelse
-        </div>
-        <div class="sidebar">
-            <div class="banner">
-                banner
-            </div>
-            <div class="topics">
-                @forelse($topics as $topic)
-                <a href="{{ route('topics.show', $topic->slug) }}">{{ $topic->name }}</a>
-                @empty
-                <p>Няма налични теми</p>
-                @endforelse
+<div>
+    <section>
+        <div class="c2 p2">
+            <img src="/images/contacts.jpg" alt="">
+        </div><div class="c2 p2">
+            <div> <img src="/images/contacts.jpg" alt=""></div>
+            <div class="c2 p1">
+                <img src="/images/contacts.jpg" alt="">
+            </div><div class="c2 p1">
+                <img src="/images/contacts.jpg" alt="">
             </div>
         </div>
-    </div>
+    </section>
 </div>
-
 @endsection
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -8,7 +8,7 @@
             <a href="/" class="fadein red">Начало</a>
             <a href="{{ route('about.show') }}" class="fadein red">За нас</a>
             <a href="/#services" class="fadein red">Услуги</a>
-            <!-- <a href="{{ route('posts.index') }}" class="fadein red">Блог</a> -->
+            <a href="{{ route('posts.index') }}" class="fadein red">Блог</a>
             <a href="{{ route('contacts.show') }}" class="fadein submit">Контакти</a>
             <div class="social">
                 <a class="fb" href="https://www.facebook.com/QACbg/" target="_blank"></a>
