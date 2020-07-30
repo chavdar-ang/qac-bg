a7987f92130103e329736f5c34fcaf11ff4cb6e7
Viki <viki.radeva91@gmail.com>
2020-07-30 16:35:53 +0300

    footer - add banner COVID

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,16 +1,17 @@
 <section class="footer">
-    <div class="c4  icon call c">
+    <div class="c5  icon call c">
         <a class="vcard"><span class="tel" itemprop="telephone">02 / 4032 400 </span></a><br/>
         <a class="vcard"><span class="tel" itemprop="telephone">02 / 4032 409 </span></a>
-    </div><div class="c4  icon location c">
+    </div><div class="c5  icon location c">
         <a href="//maps.google.com/?saddr=Current%20Location&daddr=42.666022, 23.261040&hl=bg" target="_blank">
          гр. София п.к. 1618,<br /> бул.Цар Борис III 168,<br/>
             бизнес център Андромеда,<br/>
             ет. 3, офис 31
         </a>
-    </div><div class="c4  icon mail c ">
+    </div><div class="c5  icon mail c ">
         <a href="mailto:office@qac-bg.com">email: <span itemprop="email">office@qac-bg.com</span></a><br/>
         <a href="mailto:consult@qac-bg.com">email: <span itemprop="email">consult@qac-bg.com</span></a>
-    </div><div class="c4 "><img src="/images/vizualizacia.png" alt=""></div>
+    </div><div class="c5 p1">
+    <img src="/images/vizualizacia.png" alt=""></div><div class="c5 p1"><img src="/images/vizualizacia2.png" alt=""></div>
     <div class="copyright c3">Copyright © 2008 - 2020 Кю Ей Си ООД </div> 
 </section>
